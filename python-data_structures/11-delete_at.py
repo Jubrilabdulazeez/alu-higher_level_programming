eaf689ef3f7515b1fd6c2394ee11d8b21a6d7b24
Abdulazeez Jubril <j.abdulazee@alustudent.com>
2023-03-08 06:01:38 -0800

    new commit

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,11 +1,27 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
+    """
+    Deletes the item at a specific position in a list.
+
+    Args:
+        my_list (list): The list from which an item is to be deleted.
+        idx (int): The index of the item to be deleted.
+
+    Returns:
+        list: The updated list after deleting an item at the specified index.
+    """
+    # If the list is empty, return it as it is
+    if not my_list:
+        return my_list
+
+    # If the index is negative or out of range, return the original list
     if idx < 0 or idx >= len(my_list):
         return my_list
 
-    new_list = []
-    for i in range(len(my_list)):
-        if i != idx:
-            new_list.append(my_list[i])
+    # Copy the list
+    new_list = my_list[:]
+
+    # Delete the item at the specified index
+    del new_list[idx]
 
     return new_list
