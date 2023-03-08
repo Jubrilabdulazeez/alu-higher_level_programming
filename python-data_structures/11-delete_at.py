0341a6471b849e03a61a7bfefb8766af376005a4
Abdulazeez Jubril <j.abdulazee@alustudent.com>
2023-03-08 06:20:50 -0800

    new commit

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,7 +1,10 @@
-#!/usr/bin/bash/python3
+#!/usr/bin/python3
 my_list = [1, 2, 3, 4, 5]
+
+# Remove the item at index 2
 new_list = delete_at(my_list, 2)
 print(new_list)  # Output: [1, 2, 4, 5]
 
+# Remove the item at index -1 (which is out of range)
 new_list = delete_at(my_list, -1)
 print(new_list)  # Output: [1, 2, 3, 4, 5]
