964bf12c4a86315ac20de427e92ecbf47d2172f4
Abdulazeez Jubril <j.abdulazee@alustudent.com>
2023-03-08 06:57:41 -0800

    new commit

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    # get the number of rows and columns in the matrix
+    num_rows = len(matrix)
+    num_cols = len(matrix[0]) if num_rows > 0 else 0
+
+    # create a new matrix with the same size as the input matrix
+    result = [[0 for j in range(num_cols)] for i in range(num_rows)]
+
+    # iterate over the input matrix and compute the square value of each integer
+    for i in range(num_rows):
+        for j in range(num_cols):
+            result[i][j] = matrix[i][j] ** 2
+
+    # return the resulting matrix
+    return result
