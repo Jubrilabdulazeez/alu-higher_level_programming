ddcd12773e8a7a0985da8a273dca52aea927859b
Abdulazeez Jubril <j.abdulazee@alustudent.com>
2023-03-08 06:18:58 -0800

    new commit

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,5 +1,7 @@
-#!/usr/bin/python3
-def delete_at(my_list=[], idx=0):
-    if idx < 0 or idx >= len(my_list):
-        return my_list
-    return my_list[:idx] + my_list[idx+1:]
+#!/usr/bin/bash/python3
+my_list = [1, 2, 3, 4, 5]
+new_list = delete_at(my_list, 2)
+print(new_list)  # Output: [1, 2, 4, 5]
+
+new_list = delete_at(my_list, -1)
+print(new_list)  # Output: [1, 2, 3, 4, 5]
