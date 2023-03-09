7e22f32289e4857a5c96a9b267193dc379984090
Abdulazeez Jubril <j.abdulazee@alustudent.com>
2023-03-09 03:21:29 -0800

    new commit

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    # Create a new list with the same elements as the input list
+    new_list = [elem for elem in my_list]
+
+    # Iterate through each element of the list and replace the ones that match the search element
+    for i in range(len(new_list)):
+        if new_list[i] == search:
+            new_list[i] = replace
+
+    # Return the new list with replaced elements
+    return new_list
