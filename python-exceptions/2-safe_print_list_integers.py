e3abb83a427ead81402695969cb009dea7e8dd7d
Abdulazeez Jubril <j.abdulazee@alustudent.com>
2023-03-10 01:53:09 -0800

    new commit

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-    printed = 0
-    try:
-        for i in range(x):
-            print(my_list[i], end="")
-            printed += 1
-    except IndexError:
-        pass
-    finally:
-        print()
-        return printed
+def safe_print_list_integers(my_list=[], x=0):
+    ret = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            ret += 1
+        except (ValueError, TypeError):
+            continue
+    print("")
+    return (ret)
