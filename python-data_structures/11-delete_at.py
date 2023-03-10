fc0701ac5ea4d4a19e56e1daf936efc77b48e96c
Abdulazeez Jubril <j.abdulazee@alustudent.com>
2023-03-10 02:13:44 -0800

    new commit

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
-my_list = [1, 2, 3, 4, 5]
-
-# Remove the item at index 2
-new_list = delete_at(my_list, 2)
-print(new_list)  # Output: [1, 2, 4, 5]
-
-# Remove the item at index -1 (which is out of range)
-new_list = delete_at(my_list, -1)
-print(new_list)  # Output: [1, 2, 3, 4, 5]
+def multiply_list_map(my_list=[], number=0):
+    return list(map((lambda i: i * number), my_list))
