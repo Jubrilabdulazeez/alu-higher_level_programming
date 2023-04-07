1455db0b5c66d06615648d178053dacd7899e5c8
root <j.abdulazee@alustudent.com>
2023-04-07 22:11:49 +0000

    new commit

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-# send a request to an URL with curl, and displays the size of the body of the response
-curl -s "$1" | wc -c
+
+# Get the URL from the command line argument
+url=$1
+
+# Send a GET request to the URL and save the response body to a variable
+response_body=$(curl -sSL $url)
+
+# Get the size of the response body in bytes
+body_size=$(echo -n "$response_body" | wc -c)
+
+# Display the size of the response body in bytes
+echo "The size of the response body is $body_size bytes."
