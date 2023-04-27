4857cb6a163e9e2de8adc5dde2e01efb4a5451b3
Abdulazeez Jubril <j.abdulazee@alustudent.com>
2023-04-27 08:14:17 -0700

    new commit

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,23 +1,15 @@
->>> text_indentation = __import__('5-text_indentation').text_indentation
+#!/usr/bin/python3
+""""Doc"""
 
->>> text_indentation()
-Traceback (most recent call last):
-TypeError: text_indentation() missing 1 required positional argument: 'text'
 
->>> text_indentation("Market. Analysis? How are you: Hamid")
-Market.
-<BLANKLINE>
-Analysis?
-<BLANKLINE>
-How are you:
-<BLANKLINE>
-Hamid
+def text_indentation(text):
+    """ Doc here
+    """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
 
->>> text_indentation(2)
-Traceback (most recent call last):
-TypeError: text must be a string
+    for delim in ".:?":
+        text = (delim + "\n\n").join(
+            [line.strip(" ") for line in text.split(delim)])
 
->>> text_indentation("Market. Structure")
-Market.
-<BLANKLINE>
-Structure
+    print("{}".format(text), end="")
